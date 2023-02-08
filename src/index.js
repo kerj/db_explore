c3a0c6d4e22299a4872c02b64eea6cedb0526fae
Justin <4635401+kerj@users.noreply.github.com>
2023-02-08 13:40:05 -0800

    add server reloading

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ const bp = require("body-parser");
 const app = express();
 const db = require('./queries');
 const cors = require('cors');
+// const compression = require('compression');
+// const ssr = require('./routes/ssr');
 
 const port = 3000;
+
+// app.use(compression());
+
+// app.use(express.static("public"));
 
 app.use(bp.json());
 app.use(
@@ -14,6 +20,8 @@ app.use(
 );
 
 app.use(cors());
+
+// app.use("/ssr", ssr);
 
 app.get("/", (req, res) => {
   res.json({ info: "Express/Node Postgres API" });
